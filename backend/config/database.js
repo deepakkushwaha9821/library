ea5f8fabc7d3e5a90d439fe4b27f1bedd66be1b3
deedrop <155622024+deepakkushwaha9821@users.noreply.github.com>
2026-08-07 07:24:42 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,8 @@
 const { Sequelize } = require("sequelize");
-const Database = require("better-sqlite3");
 
 const sequelize = new Sequelize({
   dialect: "sqlite",
   storage: process.env.SQLITE_PATH || "./database.sqlite",
-  dialectModule: Database,
   logging: false,
 });
 
